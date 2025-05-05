bf96aa78fe0e57952bef3cd9e037602085328e2f
Juan Arango <111590756+Vagarh@users.noreply.github.com>
2025-05-05 09:33:45 -0500

    Add files via upload
    Consultas contratos

---
+++ consulta_bd_contratos_tarifas.sql
@@ -0,0 +1,30 @@
+## conexion base de datos savia contrtos tarifas, medicamentos y tencologias
+SELECT DISTINCT  
+    cp.numero_documento, 
+    cp.razon_social,
+    c.contrato,
+    c.fecha_inicio ,
+    c.fecha_fin ,
+    CASE cd.tipo_tecnologia
+    WHEN 1 THEN 'Tecnologia'
+    WHEN 2 THEN 'Medicamento'
+    WHEN 3 THEN 'Insumo'
+    WHEN 4 THEN 'Paquete'
+    END AS Desctipo_tecnologia,
+    cd.ma_servicio_habilitacion_codigo ,
+    cd.ma_servicio_habilitacion_valor ,
+    cd.ma_tecnologia_codigo AS codigo_tecnologia,
+    cd.ma_tecnologia_valor ,
+    cd.valor_manual ,
+    cd.valor_contratado,
+    cd.complejidad ,
+    cs.mae_modalidad_contrato_valor ,
+    gu.nombre ,
+    gu.mae_region_valor 
+FROM cnt_contratos c
+INNER JOIN cnt_contrato_sedes cs ON cs.cnt_contratos_id = c.id 
+INNER JOIN cnt_contrato_detalles cd  ON cd.cnt_contratos_id = c.id AND cd.cnt_contrato_sedes_id = cs.id  -- Para identificar las sede de Capita y evento
+LEFT JOIN cnt_prestadores cp ON c.cnt_prestadores_id = cp.id 
+left join cnt_prestador_sedes cps on cs.cnt_prestador_sedes_id = cps.id 
+left join gn_ubicaciones gu on gu.id = cps.ubicacion_id 
+  where cd.activo = '1' and cd.tipo_tecnologia in ('1','4','3','2')
